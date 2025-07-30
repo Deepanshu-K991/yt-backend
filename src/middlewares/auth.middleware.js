c0f2dc89cdf032ef92d347900be9d7e55474249d
Deepanshu-K991 <deepanshukumar842@gmail.com>
2025-07-30 15:18:52 +0530

    implement user authentication with login, logout, and token refresh functionality

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.models.js"
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request")
+    }
+
+    const decoderToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findOne(decoderToken?._id).
+      select("-password -refreshToken")
+
+    if (!user) {
+      throw new ApiError(401, "Inavlid access Token");
+
+    }
+    req.user = user;
+    next()
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Inavlid access Token");
+
+  }
+
+})
